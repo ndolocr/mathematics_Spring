666b5e8b1b3b532ac1f7a715eb39af5eac7b67e4
Raphael Ndolo <raphaelndolo@EBLMICTMT2RQR0GCV.local>
2025-04-15 15:00:41 +0300

    Initializing Spring Boot Project.

---
+++ src/main/java/com/example/mathematics/MathematicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mathematics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathematicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathematicsApplication.class, args);
+	}
+
+}
